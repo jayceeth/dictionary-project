5abae52ac1edea3597075fc9552c7b18370c6f66
jayceeth <75772603+jayceeth@users.noreply.github.com>
2021-04-16 22:28:53 -0700

    Added the audio and phonetic component

--- src/Definition.js
+++ src/Definition.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meanings from "./Meanings";
+import Phonetic from "./Phonetic";
 import "./Definition.css"
 
 export default function Definition(props){
@@ -7,6 +8,13 @@ export default function Definition(props){
     return(
         <div className="Definition">
             <h2>{props.definition.word}</h2>
+            {props.definition.phonetics.map(function(phonetic, index){
+                return(
+                    <div key={index}>
+                        <Phonetic phonetic={phonetic} />
+                    </div>
+                )
+            })};
             {props.definition.meanings.map(function(meanings,index){
                 return(
                     <div key={index}>
